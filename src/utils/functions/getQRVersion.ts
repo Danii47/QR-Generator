7aaca213adf443eb7d1e6464d22ccb91fc9678a8
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-03 00:16:40 +0100

    feat: add getQRVersion function, returns the best version for a data

---
+++ src/utils/functions/getQRVersion.ts
@@ -0,0 +1,17 @@
+import { QRErrorCorrectionKey, QRVersion } from "../../types/QRTypes"
+import { QR_INFORMATION } from "../constants/QR_INFORMATION"
+
+
+export function getQRVersion(dataLength: number, correctionLevel: QRErrorCorrectionKey): QRVersion {
+  
+  for (const version in QR_INFORMATION) {
+    const versionNumber = parseInt(version) as QRVersion
+
+    const maxDataBits = QR_INFORMATION[versionNumber].eccLevels[correctionLevel].dataBits
+    if (dataLength <= maxDataBits) {
+      return versionNumber
+    }
+  }
+
+  throw new Error("Data is too long")
+}
