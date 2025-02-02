18fd8f831174d823f05d666ad0d7f597528906df
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-02 14:27:52 +0100

    data info finished

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useRef, useState } from 'react'
 import './App.css'
 import { createStartMatrix } from './utils/functions/createStartMatrix'
 import { QRMask } from './types/QRTypes'
+import { QR_INFORMATION } from './utils/constants/QR_INFORMATION'
+import { TYPE_INFORMATION_DICTIONARY } from './utils/constants/TYPE_INFORMATION_DICTIONARY'
 
 const COLORS: Record<number, string> = {
   0: "white",
@@ -19,9 +21,9 @@ const MASKS: Record<string, (i: number, j: number) => boolean> = {
   "010": (_, column) => column % 3 === 0,
   "011": (row, column) => (row + column) % 3 === 0,
   "100": (row, column) => (Math.floor(row / 2) + Math.floor(column / 3)) % 2 === 0,
-  "101": (row, column) => ((row * column) % 2) + ((row * column) % 3) === 0, //  	((row * column) mod 2) + ((row * column) mod 3) == 0
-  "110": (row, column) => (((row * column) % 2) + ((row * column) % 3)) % 2 === 0, // ( ((row * column) mod 2) + ((row * column) mod 3) ) mod 2 == 0
-  "111": (row, column) => (((row + column) % 2) + ((row * column) % 3)) % 2 === 0, // ( ((row + column) mod 2) + ((row * column) mod 3) ) mod 2 == 0
+  "101": (row, column) => ((row * column) % 2) + ((row * column) % 3) === 0,
+  "110": (row, column) => (((row * column) % 2) + ((row * column) % 3)) % 2 === 0,
+  "111": (row, column) => (((row + column) % 2) + ((row * column) % 3)) % 2 === 0
 }
 
 const MASK: QRMask = "001"
@@ -33,8 +35,10 @@ function App() {
 
   // Fill the matrix with 
   function fillNumber(binaryString: string) {
+    console.log({ binaryString, l: binaryString.length })
 
     setQRMatrix(prevQRMatrix => {
+
       const newQRMatrix = prevQRMatrix.map(row => [...row])
       let cont = false
 
@@ -56,16 +60,16 @@ function App() {
         cont = !cont
       }
 
-      for (let i = 0; i < newQRMatrix.length; i++) {
-        for (let j = 0; j < newQRMatrix[i].length; j++) {
-          if (newQRMatrix[i][j] != 2 && newQRMatrix[i][j] != 3) {
-            const mask = MASKS[MASK]
-            if (mask(i, j)) {
-              newQRMatrix[i][j] = newQRMatrix[i][j] === 0 || newQRMatrix[i][j] === 4 ? 5 : 4
-            }
-          }
-        }
-      }
+      // for (let i = 0; i < newQRMatrix.length; i++) {
+      //   for (let j = 0; j < newQRMatrix[i].length; j++) {
+      //     if (newQRMatrix[i][j] != 2 && newQRMatrix[i][j] != 3) {
+      //       const mask = MASKS[MASK]
+      //       if (mask(i, j)) {
+      //         newQRMatrix[i][j] = newQRMatrix[i][j] === 0 || newQRMatrix[i][j] === 4 ? 5 : 4
+      //       }
+      //     }
+      //   }
+      // }
 
       return newQRMatrix
     })
@@ -84,8 +88,16 @@ function App() {
   }
 
   function createQR(link: string) {
+    const dataType = "byte"
     const binaryLink = linkToBinary(link)
-    fillNumber(link.length.toString(2).padStart(8, "0") + binaryLink + "0000")
+    const binaryLinkLength = binaryLink.length.toString(2).padStart(8, "0")
+    const finalBlock = "0000"
+
+    const totalString = TYPE_INFORMATION_DICTIONARY[dataType] + binaryLinkLength + binaryLink + finalBlock
+
+    // This bytes are used to indicate the end of the data, in this case, 0xEC and 0x11, when the data 
+    const completeBytes = 0xEC.toString(2).padStart(8, "0") + 0x11.toString(2).padStart(8, "0")
+    fillNumber(totalString.padEnd(QR_INFORMATION[1].eccLevels.L.dataBits, completeBytes))
   }
 
   return (
