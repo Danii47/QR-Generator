cf347a578b9f011667572281b23f7c6c7312435a
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-04 22:59:14 +0100

    feat: add function to calculate alignment square coordinates for QR codes

---
+++ src/utils/functions/getAlignSquares.ts
@@ -0,0 +1,20 @@
+import { QRVersion } from "../../types/QRTypes";
+import { getQRDimensions } from "./getQRDimensions";
+
+export function getAlignSquaresCoords(version: QRVersion): number[] | null {
+  if (version === 1) return null
+  else {
+    const QRDimension = getQRDimensions(version)
+    const numberOfCoords = Math.floor(version / 7) + 2
+
+    // The first and last coordinates are always 6 and QRDimension - 7
+    const coordsArray = [6, QRDimension - 7]
+    const step = Math.floor((QRDimension - 13) / (numberOfCoords - 1))
+
+    for (let i = 1; i < numberOfCoords - 1; i++) {
+      coordsArray.push(6 + i * step)
+    }
+
+    return coordsArray
+  }
+}
