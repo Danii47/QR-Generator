b74366fd7a9c92dbbcbf6e88dcab82179e06c3e2
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-05 00:56:55 +0100

    feat: add get version information function

---
+++ src/utils/functions/getVersionInformation.ts
@@ -0,0 +1,13 @@
+import { QRVersion } from "../../types/QRTypes";
+import { POLYNOMIAL_GENERATOR_VERSION } from "../constants/POLYNOMIALS_GENERATORS";
+import { getCorrectionErrorBCH } from "./getCorrectionErrorBCH";
+
+export function getVersionInformation(version: QRVersion): string | null {
+  if (version < 7) return null
+
+  const binaryVersion = version.toString(2).padStart(6, "0")
+
+  const binaryVersionBits = getCorrectionErrorBCH(binaryVersion, POLYNOMIAL_GENERATOR_VERSION)
+
+  return binaryVersionBits
+}
