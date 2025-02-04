4fec9ab3fd6c99d1c832300f041b2809f8426f8c
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-05 00:56:18 +0100

    feat: add numberOfBlocksInGroupOne to qr information

--- src/utils/constants/QR_INFORMATION.ts
+++ src/utils/constants/QR_INFORMATION.ts
@@ -5,64 +5,64 @@ export const QR_INFORMATION: Record<QRVersion, QRInformation> = {
   1: {
     modules: "21x21",
     eccLevels: {
-      "L": { dataBits: 152, numeric: 41, alphanumeric: 25, binary: 17, kanji: 10 },
-      "M": { dataBits: 128, numeric: 34, alphanumeric: 20, binary: 14, kanji: 8 },
-      "Q": { dataBits: 104, numeric: 27, alphanumeric: 16, binary: 11, kanji: 7 },
-      "H": { dataBits: 72, numeric: 17, alphanumeric: 10, binary: 7, kanji: 4 }
+      "L": { dataBits: 152, numeric: 41, alphanumeric: 25, binary: 17, kanji: 10, numberOfBlocksInGroupOne: 1 },
+      "M": { dataBits: 128, numeric: 34, alphanumeric: 20, binary: 14, kanji: 8, numberOfBlocksInGroupOne: 1 },
+      "Q": { dataBits: 104, numeric: 27, alphanumeric: 16, binary: 11, kanji: 7, numberOfBlocksInGroupOne: 1 },
+      "H": { dataBits: 72, numeric: 17, alphanumeric: 10, binary: 7, kanji: 4, numberOfBlocksInGroupOne: 1 }
     }
   },
   2: {
     modules: "25x25",
     eccLevels: {
-      "L": { dataBits: 272, numeric: 77, alphanumeric: 47, binary: 32, kanji: 20 },
-      "M": { dataBits: 224, numeric: 63, alphanumeric: 38, binary: 26, kanji: 16 },
-      "Q": { dataBits: 176, numeric: 48, alphanumeric: 29, binary: 20, kanji: 12 },
-      "H": { dataBits: 128, numeric: 34, alphanumeric: 20, binary: 14, kanji: 8 }
+      "L": { dataBits: 272, numeric: 77, alphanumeric: 47, binary: 32, kanji: 20, numberOfBlocksInGroupOne: 1 },
+      "M": { dataBits: 224, numeric: 63, alphanumeric: 38, binary: 26, kanji: 16, numberOfBlocksInGroupOne: 1 },
+      "Q": { dataBits: 176, numeric: 48, alphanumeric: 29, binary: 20, kanji: 12, numberOfBlocksInGroupOne: 1 },
+      "H": { dataBits: 128, numeric: 34, alphanumeric: 20, binary: 14, kanji: 8, numberOfBlocksInGroupOne: 1 }
     }
   },
   3: {
     modules: "29x29",
     eccLevels: {
-      "L": { dataBits: 440, numeric: 127, alphanumeric: 77, binary: 53, kanji: 32 },
-      "M": { dataBits: 352, numeric: 101, alphanumeric: 61, binary: 42, kanji: 26 },
-      "Q": { dataBits: 272, numeric: 77, alphanumeric: 47, binary: 32, kanji: 20 },
-      "H": { dataBits: 208, numeric: 58, alphanumeric: 35, binary: 24, kanji: 15 }
+      "L": { dataBits: 440, numeric: 127, alphanumeric: 77, binary: 53, kanji: 32, numberOfBlocksInGroupOne: 1 },
+      "M": { dataBits: 352, numeric: 101, alphanumeric: 61, binary: 42, kanji: 26, numberOfBlocksInGroupOne: 1 },
+      "Q": { dataBits: 272, numeric: 77, alphanumeric: 47, binary: 32, kanji: 20, numberOfBlocksInGroupOne: 2 },
+      "H": { dataBits: 208, numeric: 58, alphanumeric: 35, binary: 24, kanji: 15, numberOfBlocksInGroupOne: 2 }
     }
   },
   4: {
     modules: "33x33",
     eccLevels: {
-      "L": { dataBits: 640, numeric: 187, alphanumeric: 114, binary: 78, kanji: 48 },
-      "M": { dataBits: 512, numeric: 149, alphanumeric: 90, binary: 62, kanji: 38 },
-      "Q": { dataBits: 384, numeric: 111, alphanumeric: 67, binary: 46, kanji: 28 },
-      "H": { dataBits: 288, numeric: 82, alphanumeric: 50, binary: 34, kanji: 21 }
+      "L": { dataBits: 640, numeric: 187, alphanumeric: 114, binary: 78, kanji: 48, numberOfBlocksInGroupOne: 1 },
+      "M": { dataBits: 512, numeric: 149, alphanumeric: 90, binary: 62, kanji: 38, numberOfBlocksInGroupOne: 2 },
+      "Q": { dataBits: 384, numeric: 111, alphanumeric: 67, binary: 46, kanji: 28, numberOfBlocksInGroupOne: 2 },
+      "H": { dataBits: 288, numeric: 82, alphanumeric: 50, binary: 34, kanji: 21, numberOfBlocksInGroupOne: 4 }
     }
   },
   5: {
     modules: "37x37",
     eccLevels: {
-      "L": { dataBits: 864, numeric: 255, alphanumeric: 154, binary: 106, kanji: 65 },
-      "M": { dataBits: 688, numeric: 202, alphanumeric: 122, binary: 84, kanji: 52 },
-      "Q": { dataBits: 496, numeric: 144, alphanumeric: 87, binary: 60, kanji: 37 },
-      "H": { dataBits: 368, numeric: 106, alphanumeric: 64, binary: 44, kanji: 27 }
+      "L": { dataBits: 864, numeric: 255, alphanumeric: 154, binary: 106, kanji: 65, numberOfBlocksInGroupOne: 1 },
+      "M": { dataBits: 688, numeric: 202, alphanumeric: 122, binary: 84, kanji: 52, numberOfBlocksInGroupOne: 2 },
+      "Q": { dataBits: 496, numeric: 144, alphanumeric: 87, binary: 60, kanji: 37, numberOfBlocksInGroupOne: 2 },
+      "H": { dataBits: 368, numeric: 106, alphanumeric: 64, binary: 44, kanji: 27, numberOfBlocksInGroupOne: 2 }
     }
   },
   6: {
     modules: "41x41",
     eccLevels: {
-      "L": { dataBits: 1088, numeric: 322, alphanumeric: 195, binary: 134, kanji: 82 },
-      "M": { dataBits: 864, numeric: 255, alphanumeric: 154, binary: 106, kanji: 65 },
-      "Q": { dataBits: 608, numeric: 178, alphanumeric: 108, binary: 74, kanji: 45 },
-      "H": { dataBits: 480, numeric: 139, alphanumeric: 84, binary: 58, kanji: 36 }
+      "L": { dataBits: 1088, numeric: 322, alphanumeric: 195, binary: 134, kanji: 82, numberOfBlocksInGroupOne: 2 },
+      "M": { dataBits: 864, numeric: 255, alphanumeric: 154, binary: 106, kanji: 65, numberOfBlocksInGroupOne: 4 },
+      "Q": { dataBits: 608, numeric: 178, alphanumeric: 108, binary: 74, kanji: 45, numberOfBlocksInGroupOne: 4 },
+      "H": { dataBits: 480, numeric: 139, alphanumeric: 84, binary: 58, kanji: 36, numberOfBlocksInGroupOne: 4 }
     }
   },
   7: {
     modules: "45x45",
     eccLevels: {
-      "L": { dataBits: 1248, numeric: 370, alphanumeric: 224, binary: 154, kanji: 95 },
-      "M": { dataBits: 992, numeric: 293, alphanumeric: 178, binary: 122, kanji: 75 },
-      "Q": { dataBits: 704, numeric: 207, alphanumeric: 125, binary: 86, kanji: 53 },
-      "H": { dataBits: 528, numeric: 154, alphanumeric: 93, binary: 64, kanji: 39 }
+      "L": { dataBits: 1248, numeric: 370, alphanumeric: 224, binary: 154, kanji: 95, numberOfBlocksInGroupOne: 2 },
+      "M": { dataBits: 992, numeric: 293, alphanumeric: 178, binary: 122, kanji: 75, numberOfBlocksInGroupOne: 4 },
+      "Q": { dataBits: 704, numeric: 207, alphanumeric: 125, binary: 86, kanji: 53, numberOfBlocksInGroupOne: 2 },
+      "H": { dataBits: 528, numeric: 154, alphanumeric: 93, binary: 64, kanji: 39, numberOfBlocksInGroupOne: 4 }
     }
   },
   8: {
