96793dadcdeec0ce06f804744a898d112c179b2a
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-04 22:57:25 +0100

    fix: qr patterns

--- src/utils/constants/QR_PATTERNS.ts
+++ src/utils/constants/QR_PATTERNS.ts
@@ -6,7 +6,7 @@ export const PATTERNS_FUNCTIONS: Record<string, (i: number, j: number) => boolea
   "010": (_, column) => column % 3 === 0,
   "011": (row, column) => (row + column) % 3 === 0,
   "100": (row, column) => (Math.floor(row / 2) + Math.floor(column / 3)) % 2 === 0,
-  "101": (row, column) => ((row * column) % 2) + ((row * column) % 3) === 0,
-  "110": (row, column) => (((row * column) % 2) + ((row * column) % 3)) % 2 === 0,
-  "111": (row, column) => (((row + column) % 2) + ((row * column) % 3)) % 2 === 0
+  "101": (row, column) => (row * column) % 2 + (row * column) % 3 === 0,
+  "110": (row, column) => ((row * column) % 3 + row * column) % 2 === 0,
+  "111": (row, column) => ((row * column) % 3 + row + column) % 2 === 0
 }
