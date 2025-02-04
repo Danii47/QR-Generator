e2a18a49e5ccebf4a2c46e920487c60ab24cfa91
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-04 23:08:27 +0100

    feat: update QR code generation logic and improve coordinate grid handling

--- src/utils/functions/getCoordinateGrid.ts
+++ src/utils/functions/getCoordinateGrid.ts
@@ -2,17 +2,17 @@ import { QRVersion } from "../../types/QRTypes";
 import { getAlignSquaresCoords } from "./getAlignSquares";
 import { getQRDimensions } from "./getQRDimensions";
 
-export function getCoordinateGrid(version: QRVersion) {
+export function getCoordinateGrid(version: QRVersion): number[][] {
   const coordsArray = getAlignSquaresCoords(version)
-
-  if (!coordsArray) return null
 
   const QRDimension = getQRDimensions(version)
   const grid = []
 
   for (let i = 0; i < coordsArray.length; i++) {
     for (let j = 0; j < coordsArray.length; j++) {
-      if ((coordsArray[i] === 6 && coordsArray[j] === 6) || (coordsArray[i] === QRDimension - 7 && coordsArray[j] === 6) || (coordsArray[i] === 6 && coordsArray[j] === QRDimension - 7)) continue
+      // Avoid the squares that are in the finder patterns
+      if ((coordsArray[i] === 6 && coordsArray[j] === 6) || (coordsArray[i] === QRDimension - 7 && coordsArray[j] === 6) || (coordsArray[i] === 6 && coordsArray[j] === QRDimension - 7))
+        continue
       
       grid.push([coordsArray[i], coordsArray[j]])
     }
