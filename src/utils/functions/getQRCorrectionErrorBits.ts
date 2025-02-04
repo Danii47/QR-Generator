83c833d7c44169c79195ccec15134f1013c9d742
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-05 00:56:35 +0100

    feat: actualizar cálculo de bits de corrección de QR para incluir numberOfBlocksInGroupOne

--- src/utils/functions/getQRCorrectionErrorBits.ts
+++ src/utils/functions/getQRCorrectionErrorBits.ts
@@ -4,9 +4,9 @@ import { getQRTotalBits } from "./getQRTotalBits";
 
 export function getQRCorrectionErrorBits(version: QRVersion, errorCorrectionLevel: QRErrorCorrectionKey): { correctionBits: number, correctionBytes: number } {
   const { totalBits } = getQRTotalBits(version)
-  const dataBits = QR_INFORMATION[version].eccLevels[errorCorrectionLevel].dataBits
+  const { dataBits, numberOfBlocksInGroupOne } = QR_INFORMATION[version].eccLevels[errorCorrectionLevel]
 
   const correctionBits = totalBits - dataBits
 
-  return { correctionBits, correctionBytes: Math.floor(correctionBits / 8) }
+  return { correctionBits: Math.floor(correctionBits / numberOfBlocksInGroupOne), correctionBytes: Math.floor((correctionBits / 8) / numberOfBlocksInGroupOne) }
 }
