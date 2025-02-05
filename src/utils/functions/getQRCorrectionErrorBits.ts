e547d1d2288b322929e421d125d4eb10d09c2ac8
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-06 00:48:00 +0100

    feat: add group two in get correction error bits

--- src/utils/functions/getQRCorrectionErrorBits.ts
+++ src/utils/functions/getQRCorrectionErrorBits.ts
@@ -4,9 +4,10 @@ import { getQRTotalBits } from "./getQRTotalBits";
 
 export function getQRCorrectionErrorBits(version: QRVersion, errorCorrectionLevel: QRErrorCorrectionKey): { correctionBits: number, correctionBytes: number } {
   const { totalBits } = getQRTotalBits(version)
-  const { dataBits, numberOfBlocksInGroupOne } = QR_INFORMATION[version].eccLevels[errorCorrectionLevel]
+  const { dataBits, numberOfBlocksInGroupOne, numberOfBlocksInGroupTwo } = QR_INFORMATION[version].eccLevels[errorCorrectionLevel]
 
   const correctionBits = totalBits - dataBits
+  const totalBlocks = numberOfBlocksInGroupOne + numberOfBlocksInGroupTwo
 
-  return { correctionBits: Math.floor(correctionBits / numberOfBlocksInGroupOne), correctionBytes: Math.floor((correctionBits / 8) / numberOfBlocksInGroupOne) }
+  return { correctionBits: Math.floor(correctionBits / totalBlocks), correctionBytes: Math.floor((correctionBits / 8) / totalBlocks) }
 }
