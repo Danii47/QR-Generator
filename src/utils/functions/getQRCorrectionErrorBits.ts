80ed7968ad0a0adb95e11da6fad8bf93e2793575
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-08 16:55:42 +0100

    feat: add missing line for better readability in getQRCorrectionErrorBits function

--- src/utils/functions/getQRCorrectionErrorBits.ts
+++ src/utils/functions/getQRCorrectionErrorBits.ts
@@ -1,9 +1,10 @@
-import { QRErrorCorrectionKey, QRVersion } from "../../types/QRTypes";
-import { QR_INFORMATION } from "../constants/QR_INFORMATION";
-import { getQRTotalBits } from "./getQRTotalBits";
+import { QRErrorCorrectionKey, QRVersion } from "../../types/QRTypes"
+import { QR_INFORMATION } from "../constants/QR_INFORMATION"
+import { getQRTotalBits } from "./getQRTotalBits"
 
 export function getQRCorrectionErrorBits(version: QRVersion, errorCorrectionLevel: QRErrorCorrectionKey): { correctionBits: number, correctionBytes: number } {
   const { totalBits } = getQRTotalBits(version)
+
   const { dataBits, numberOfBlocksInGroupOne, numberOfBlocksInGroupTwo } = QR_INFORMATION[version].eccLevels[errorCorrectionLevel]
 
   const correctionBits = totalBits - dataBits
