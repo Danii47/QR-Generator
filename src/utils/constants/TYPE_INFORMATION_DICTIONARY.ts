2313c937944a76fd5fc7cb62a5d8f7e5e4b581ef
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-08 16:58:48 +0100

    feat: update TYPE_INFORMATION_DICTIONARY to use QREncodedType for improved type handling

--- src/utils/constants/TYPE_INFORMATION_DICTIONARY.ts
+++ src/utils/constants/TYPE_INFORMATION_DICTIONARY.ts
@@ -1,6 +1,6 @@
-import { QRDataType, QRDataTypeFlag } from "../../types/QRTypes";
+import { QREncodedType, QREncodedTypeFlag } from "../../types/QRTypes";
 
-export const TYPE_INFORMATION_DICTIONARY: Record<QRDataType, QRDataTypeFlag> = {
+export const TYPE_INFORMATION_DICTIONARY: Record<QREncodedType, QREncodedTypeFlag> = {
   "numeric": "0001",
   "alphanumeric": "0010",
   "byte": "0100",
