c2a2568b880eedd01347eb185a3851b99a503c90
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-08 16:56:29 +0100

    feat: add encoded type parameter to getQRVersion function for improved error handling

--- src/utils/functions/getQRVersion.ts
+++ src/utils/functions/getQRVersion.ts
@@ -1,13 +1,15 @@
-import { QRErrorCorrectionKey, QRVersion } from "../../types/QRTypes"
+import { QREncodedType, QRErrorCorrectionKey, QRVersion } from "../../types/QRTypes"
 import { QR_INFORMATION } from "../constants/QR_INFORMATION"
 
-export function getQRVersion(dataLength: number, correctionLevel: QRErrorCorrectionKey): QRVersion {
-  
+export function getQRVersion(dataLength: number, correctionLevel: QRErrorCorrectionKey, encodedType: QREncodedType): QRVersion {
+  if (QR_INFORMATION[1].eccLevels[correctionLevel][encodedType] === undefined)
+    throw new Error("Invalid encoded type")
+
   for (const version in QR_INFORMATION) {
     const versionNumber = parseInt(version) as QRVersion
 
-    const maxDataBits = QR_INFORMATION[versionNumber].eccLevels[correctionLevel].dataBits
-    if (dataLength <= maxDataBits) {
+    const maxDataBytes = QR_INFORMATION[versionNumber].eccLevels[correctionLevel][encodedType]
+    if (dataLength / 8 <= maxDataBytes) {
       return versionNumber
     }
   }
