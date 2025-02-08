6f075046ebe82ff4e59e6564cd18600f1bd3dfce
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-08 16:55:38 +0100

    feat: add getLengthBits function for QR code length calculation

---
+++ src/utils/functions/getLengthBits.ts
@@ -0,0 +1,24 @@
+import { QREncodedType, QRVersion } from "../../types/QRTypes";
+
+export function getLengthBits(version: QRVersion, encodedType: QREncodedType) {
+  switch (encodedType) {
+    case "numeric":
+      if (version <= 9) return 10
+      if (version <= 26) return 12
+      return 14
+    case "alphanumeric":
+      if (version <= 9) return 9
+      if (version <= 26) return 11
+      return 13
+    case "byte":
+      if (version <= 9) return 8
+      if (version <= 26) return 16
+      return 16
+    case "kanji":
+      if (version <= 9) return 8
+      if (version <= 26) return 10
+      return 12
+    default:
+      throw new Error("Invalid encoded type")
+  }
+}
